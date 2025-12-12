762443015dab31589eacbe3320e87aa4558d1e6a
intelconstructlab <77456598+intelconstructlab@users.noreply.github.com>
2025-12-12 04:48:36 +0000

    Deploying to gh-pages from @ intelconstructlab/openconstructionrobotics@af8b692dc01f200620e7f116cab333655683e204 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/openconstructionrobotics/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/openconstructionrobotics/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-we-just-release-our-website",
+          title: 'We just release our website!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

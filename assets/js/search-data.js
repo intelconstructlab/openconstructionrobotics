6057ff192673845b568c7fbb9ecf3e8098f69e97
intelconstructlab <77456598+intelconstructlab@users.noreply.github.com>
2025-12-16 08:52:53 +0000

    Deploying to gh-pages from @ intelconstructlab/openconstructionrobotics@36b26373df05031a28a72249310abbf19f6fa7ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,9 +427,9 @@ ninja.data = [{
           description: "a robot for thermal inspection in ceilings",
           section: "Projects",handler: () => {
               window.location.href = "/openconstructionrobotics/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-bim2ros",
+          title: 'bim2ros',
+          description: "a toolkit that integrates BIM with ROS.",
           section: "Projects",handler: () => {
               window.location.href = "/openconstructionrobotics/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",

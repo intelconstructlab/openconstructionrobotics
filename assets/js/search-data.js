2bdc238a948297f5b439c76457bff2f42a76cdef
intelconstructlab <77456598+intelconstructlab@users.noreply.github.com>
2025-12-16 07:51:41 +0000

    Deploying to gh-pages from @ intelconstructlab/openconstructionrobotics@49c2d6c51621498458e8bbf24f2b30800f149161 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Open-source construction robotics projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/openconstructionrobotics/projects/";
